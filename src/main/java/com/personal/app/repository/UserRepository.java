fa8c305834b9fc92e66807b4ba981894b4b7523e
MITHUN Y <125362463+Mithun1639@users.noreply.github.com>
2025-10-25 20:52:19 +0530

    modified

--- src/main/java/com/personal/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.personal.app.repository;
-import com.personal.app.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
-
-    User findByAccountNumber(String accountNum);
-}
